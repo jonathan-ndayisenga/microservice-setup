f3f5f6a584a5f53e2bb5765d25b859fd6805e80f
jonathan-ndayisenga <brucendayisenga22@gmail.com>
2023-09-06 13:38:29 +0300

    Add models.py file

--- models.py
+++ models.py
@@ -27,15 +27,93 @@ class Product(Base):
     # Add other product-related fields
     
     
-    from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 # Create an SQLAlchemy session
 engine = create_engine('database_connection_string')
 Session = sessionmaker(bind=engine)
 session = Session()
 
+Base = declarative_base()
+
+# Define the Order model
+class Order(Base):
+    __tablename__ = 'orders'
+    
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer)
+    product_id = Column(Integer)
+    quantity = Column(Integer)
+
+# Create the table if it doesn't exist
+Base.metadata.create_all(engine)
+
 # Create a new order
-new_order = Order(customer_id=1, product_id=2, quantity=3)
-session.add(new_order)
-session.commit()
+def create_order(customer_id, product_id, quantity):
+    new_order = Order(customer_id=customer_id, product_id=product_id, quantity=quantity)
+    session.add(new_order)
+    session.commit()
+
+# Read an order by ID
+def read_order(order_id):
+    order = session.query(Order).filter(Order.id == order_id).first()
+    return order
+
+# Update an order by ID
+def update_order(order_id, customer_id, product_id, quantity):
+    order = session.query(Order).filter(Order.id == order_id).first()
+    if order:
+        order.customer_id = customer_id
+        order.product_id = product_id
+        order.quantity = quantity
+        session.commit()
+
+# Delete an order by ID
+def delete_order(order_id):
+    order = session.query(Order).filter(Order.id == order_id).first()
+    if order:
+        session.delete(order)
+        session.commit()
+
+# Usage examples:
+
+# Create a new order
+create_order(customer_id=1, product_id=2, quantity=3)
+
+# Read an order
+order = read_order(order_id=1)
+if order:
+    print(f"Order ID: {order.id}, Customer ID: {order.customer_id}, Product ID: {order.product_id}, Quantity: {order.quantity}")
+
+# Update an order
+update_order(order_id=1, customer_id=2, product_id=3, quantity=5)
+
+# Delete an order
+delete_order(order_id=1)
+
+
+
+
+#Create API Endpoint /orders/request
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/orders/request', methods=['POST'])
+def create_order():
+    data = request.json
+
+    # Extract order details from JSON data
+    shopping_cart_id = data.get('shopping_cart_id')
+
+    # Perform order creation and location determination logic here
+
+    # Return a response indicating success
+    return jsonify({"message": "Order created successfully"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
