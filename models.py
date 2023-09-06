81ef4bb357ce1cc27d6faaaf3a68526ebd0e502b
jonathan-ndayisenga <brucendayisenga22@gmail.com>
2023-09-06 13:21:38 +0300

    Add models.py file

--- models.py
+++ models.py
@@ -25,3 +25,17 @@ class Product(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     # Add other product-related fields
+    
+    
+    from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Create an SQLAlchemy session
+engine = create_engine('database_connection_string')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Create a new order
+new_order = Order(customer_id=1, product_id=2, quantity=3)
+session.add(new_order)
+session.commit()
